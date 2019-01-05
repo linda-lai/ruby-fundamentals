089729cd7cb72306f26685282490f0c50db37965
Linda Lai <lindalai.615@gmail.com>
2019-01-05 23:49:02 +1100

    Add Ruby fundamentals q17

---
+++ ruby-fundamentals-q17.rb
@@ -0,0 +1,16 @@
+# *Problem 17*
+# You took a job in Shanghai along with Ruegen working for a financial services company.
+# Your boss is Chinese (obviously), a bit of a madman, and dislikes the way the West has 'taken over'.
+# He wants you to write a currency exchange method, but each currency must only be compared to the Chinese renminbi.
+
+# Write some methods, each that take a number representing a currency (let's say Thai baht, or Indonesian rupiah, and whatever you like),
+# and converts each into the number of Chinese renminbi.
+
+# Now write a method that takes a number, and two strings.
+
+# Using only the methods you have created, code this last method so that you can specify 'from' and 'to' currencies (string arguments), and a number,
+# and have this return the relevant exchange.
+
+# For example, you could accept 'vnd' (the Viet dong), 'hkd' (Honky dollar), and a number, and gives the correct exchange for that number given those country parameters.
+
+# Please store I a w-n variable!
