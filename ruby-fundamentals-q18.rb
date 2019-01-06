d9384b92775a2012ce9d907b5dfc1dea62a581c4
Linda Lai <lindalai.615@gmail.com>
2019-01-06 22:44:20 +1100

    Add Ruby fundamentals q18

---
+++ ruby-fundamentals-q18.rb
@@ -0,0 +1,6 @@
+# *Problem 18*
+# Write a method that takes two arguments, an array and a string.
+
+# If the string is not in the array it returns nil, and if it does contain the string, it returns the index of the string within the array.
+
+# You will use this in the following question.
